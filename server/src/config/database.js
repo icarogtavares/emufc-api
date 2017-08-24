d4222a964742a0f34c17d9ebb4b40f53fe9eb0f5
Icaro Tavares <icarotavares@live.com>
2017-08-24 17:16:04 -0300

    Database config

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,14 +4,14 @@ let config = getConfig({
     development: {
         username: process.env.DB_USERNAME || "root",
         password: process.env.DB_PASSWORD || "root",
-        database: process.env.DB_DATABASE || "emufc",
+        database: process.env.DB_NAME || "emufc",
         host: process.env.DB_HOST || "127.0.0.1",
         dialect: "mysql"
     },
     production: {
         username: process.env.DB_USERNAME || "root",
         password: process.env.DB_PASSWORD || "root",
-        database: process.env.DB_DATABASE || "emufc",
+        database: process.env.DB_NAME || "emufc",
         host: process.env.DB_HOST || "127.0.0.1",
         dialect: "mysql",
         logging: false
@@ -19,7 +19,7 @@ let config = getConfig({
     test: {
         username: process.env.DB_USERNAME || "root",
         password: process.env.DB_PASSWORD || "root",
-        database: process.env.DB_DATABASE || "emufc_test",
+        database: process.env.DB_NAME || "emufc_test",
         host: process.env.DB_HOST || "127.0.0.1",
         dialect: "mysql",
         logging: false
