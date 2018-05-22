e905f21ba897a0b2f40e36e1e75b9cde164c64c1
Icaro Tavares <icaro.tavares@secrel.net.br>
2018-05-22 18:40:12 -0300

    Adicionado eslint, Falta configurar

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+//module.exports = {
+//     "extends": "standard"
+// };
+
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "rules": {
+    "import/prefer-default-export": false,
+    "import/no-extraneous-dependencies": false,
+    "import/no-unresolved": false,
+    "import/extensions": false
+  }
+}
