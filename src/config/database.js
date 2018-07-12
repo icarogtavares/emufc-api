d68bdf6193aabceefcfb39af46d672152492a42e
Icaro Tavares <icarotav4@gmail.com>
2018-07-12 17:44:53 -0300

    Fixed database password env var
    Issue that the server cannot connect to the server if the password is an empty string

--- src/config/database.js
+++ src/config/database.js
@@ -2,24 +2,24 @@ const { getConfig } = require('./index')
 
 const config = getConfig({
   development: {
-    username: process.env.DB_USERNAME || 'icaro',
-    password: process.env.DB_PASSWORD || 'root',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'emufc',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME || 'icaro',
-    password: process.env.DB_PASSWORD || 'root',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'emufc',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false,
   },
   test: {
-    username: process.env.TEST_DB_USERNAME || 'icaro',
-    password: process.env.TEST_DB_PASSWORD || 'root',
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
     database: process.env.TEST_DB_NAME || 'emufc_test',
     host: process.env.TEST_DB_HOST || '127.0.0.1',
     dialect: 'mysql',
